d1c55d7b789f23578048273f54f70053cbac9e51
Mateen Nawaz <mateenNawaz86>
2025-06-02 07:57:01 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // ensure base path
+  build: {
+    outDir: "dist", // default is fine, just confirm it's not misconfigured
+  },
+});
